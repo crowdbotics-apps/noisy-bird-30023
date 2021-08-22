cba60aef4a9b95953fccf01564b3b6c6d0bf2f5d
Crowdbotics <team@crowdbotics.com>
2021-08-22 16:54:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "noisy_bird_30023Identifier",
+  url: "https://noisy_bird_30023Identifier.botics.co",
+  api: "https://noisy_bird_30023Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
